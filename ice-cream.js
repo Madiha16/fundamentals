e427ac8265d23f1d59fbe49b3bb9cb1786622a7e
Your Name <your@email.com>
2021-11-04 23:43:00 -0400

    Add packing-files

---
+++ ice-cream.js
@@ -0,0 +1,17 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+//add root beet to array
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//get first flavour
+console.log(iceCreamFlavours[0]);
+
+//get last flavour
+console.log(iceCreamFlavours[5]);
+
+//total number of flavours
+console.log(iceCreamFlavours.length);
